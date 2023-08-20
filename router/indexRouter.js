c00c320026b4dcbcdd0702788f921a3e5b5f6dab
LuuGolis <golischevskyl@gmail.com>
2023-08-20 06:54:01 -0300

    Fix

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -6,4 +6,8 @@ indexRouter.get('/', (request, response, next) =>{
     response.send('Bienvenido a mi server en /api :)')
 })
 
+indexRouter.get('/home', (request, response, next) =>{
+    response.send('Bienvenido a mi server en /home :)')
+})
+
 export default indexRouter
