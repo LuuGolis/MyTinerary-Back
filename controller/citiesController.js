c375c6f59683d7678e8528ca12909a6d63e51686
LuuGolis <golischevskyl@gmail.com>
2023-08-22 21:32:11 -0300

    controller - falta upd y del

--- controller/citiesController.js
+++ controller/citiesController.js
@@ -3,83 +3,78 @@ import cities from "../cities.js";
 import City from "../Models/City.js"
 
 const citiesController = {
-    getAllCities:async(request, response, next) => {
-      /*  const allCities = await City.find({
-            name:
-        })*/
-        let cities
-        try {
-          cities = await City.find()    
-        
-            response.json({
-                //response:City,
-                success:true,
-                error:null
-            })
-        
-           } catch (error) {
-            console.log(error);
-        
-            response.json({
-                response:null,
-                success:false,
-                error:error
-            })
-        
-           }
-    },
-    getOneCity:async(request, response, next) => {
-        console.log(request.params);
-        const { _name } = request.params
-        let cities ;
+    getAllCities: async (request, response, next) => {
+        /*  const allCities = await City.find({
+              name:
+          })*/
+        let cities;
         let error = null
         let success = true;
- 
         try {
-            cities = await City.find({name: _name})
+            cities = await City.find()
+
+            response.json({
+                response: cities,
+                success,
+                error
+            })
+
+        } catch (err) {
+
+            console.log(err);
+            success = false;
+            error = err;
+        }
+    },
+    getOneCity: async (request, response, next) => {
+        console.log(request.params);
+        const { _name } = request.params
+        let cities;
+        let error = null
+        let success = true;
+
+        try {
+            cities = await City.findOne({ name: _name })
         } catch (err) {
             console.log(err);
             success = false;
             error = err;
         }
         response.json({
-            response:cities,
+            response: cities,
             success,
             error
         })
-},
-createOneCity:async (request, response, next)=>{
-   console.log(request.body);
+    },
+    createOneCity: async (request, response, next) => {
+        console.log(request.body);
+        let cities;
+        let error = null
+        let success = true;
+        try {
+            /* const newCity = new City(request.body)
+             
+             await newCity.save()
+             console.log(newCity);*/
+            city = await City.create(request.body)
+            console.log(city);
+            //await City.create(request.body)
+        } catch (err) {
+            console.log(err);
+            success = false;
+            error = err;
+        }
+        response.json({
+            response: cities,
+            success,
+            error
+        })
 
-   try {
-    const newCity = new City(request.body)
-    
-    await newCity.save()
-    console.log(newCity);
-
-    //await City.create(request.body)
+    }
 
 
-    response.json({
-        //response:City,
-        success:true,
-        error:null
-    })
-
-   } catch (error) {
-    console.log(error);
-
-    response.json({
-        response:null,
-        success:false,
-        error:error
-    })
-
-   }
-    
-   
 }
-}
+
 
 
 
--- router/citiesRouter.js
+++ router/citiesRouter.js
@@ -3,7 +3,7 @@ import citiesController from "../controller/citiesController.js";
 
 const citiesRouter = Router()
 
-citiesRouter.get('', citiesController.getAllCities)
+citiesRouter.get('/', citiesController.getAllCities)
 citiesRouter.get('/:name', citiesController.getOneCity)
 citiesRouter.post('/', citiesController.createOneCity)
 
